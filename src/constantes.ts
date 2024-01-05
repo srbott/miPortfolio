1ad3638eabef4c0439eb7121f18d018bbe4ca4a5
srbott <andrespunto96@hotmail.es>
2024-01-05 01:15:42 +0100

    maquetando

---
+++ src/constantes.ts
@@ -0,0 +1,91 @@
+import type { technology } from "./components/TeckStackList.astro";
+
+export const ECCStack: technology[] = [
+  {
+    name: "Java Versión 1.8",
+    icon: ""
+  },
+  {
+    name: "JPA (EclipseLink)",
+    icon: ""
+  },
+  {
+    name: "JSF",
+    icon: ""
+  },
+  {
+    name: "Mockito",
+    icon: ""
+  },
+  {
+    name: "Selenium",
+    icon: ""
+  },
+  {
+    name: "SQL (Oracle)",
+    icon: ""
+  },
+  {
+    name: "PLSQL (Oracle)",
+    icon: ""
+  },
+  {
+    name: "SOAP",
+    icon: ""
+  },
+  {
+    name: "REST",
+    icon: ""
+  },
+  {
+    name: "Lit Versión 1",
+    icon: ""
+  },
+  {
+    name: "Weblogic",
+    icon: ""
+  },
+  {
+    name: "Eclipse",
+    icon: ""
+  }
+];
+
+export const InjuriesStack: technology[] = [
+  {
+    name: "Typescript",
+    icon: ""
+  },
+  {
+    name: "Lit versiones 2 y 3",
+    icon: ""
+  },
+  {
+    name: "Openwc/testing",
+    icon: ""
+  },
+  {
+    name: "Nginx",
+    icon: ""
+  },
+  {
+    name: "Npm",
+    icon: ""
+  },
+  {
+    name: "JDocs",
+    icon: ""
+  },
+  {
+    name: "Storybook",
+    icon: ""
+  },
+  {
+    name: "REST",
+    icon: ""
+  },
+  {
+    name: "Java 1.17",
+    icon: ""
+  }
+]
